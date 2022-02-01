fcf4181602253b3f11d608e1ba354dc4f659fc67
Mel <meljohnpualon@gmail.com>
2022-02-01 19:31:08 +0800

    added users data

---
+++ routes/users.js
@@ -0,0 +1,19 @@
+import express from "express";
+
+const router = express.Router();
+
+const users = [
+    {
+        firstName: "John",
+        lastName: "Doe",
+        age: 25
+    }
+]
+
+router.get('/', (req, res) => {
+
+    console.log(users)
+    res.send('users hello')
+});
+
+export default router
